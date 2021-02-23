ed43490ff50853e7e561ec0b738a95d24711906e
Jayna Leitze <jayna.leitze@gmail.com>
2021-02-23 09:40:24 -0600

    Update all provider paths to localhost:8000

--- src/components/comments/CommentProvider.js
+++ src/components/comments/CommentProvider.js
@@ -1,65 +1,74 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
-export const CommentContext = React.createContext()
+export const CommentContext = React.createContext();
 
 export const CommentProvider = (props) => {
-    const [ comments, setComments ] = useState([])
-    const [ relatedComments, setRelatedComments ] = useState([])
-    const [ comment, setComment ] = useState({})
+  const [comments, setComments] = useState([]);
+  const [relatedComments, setRelatedComments] = useState([]);
+  const [comment, setComment] = useState({});
 
-    const addComment = (comments) => {
-        return fetch("http://localhost:8088/comments", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(comments)
-        })
-        .then(getComments)
-    }
+  const addComment = (comments) => {
+    return fetch("http://localhost:8000/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(comments),
+    }).then(getComments);
+  };
 
-    const getComments = () => {
-        return fetch('http://localhost:8088/comments')
-        .then(res => res.json())
-        .then(setComments)
-    }
+  const getComments = () => {
+    return fetch("http://localhost:8000/comments")
+      .then((res) => res.json())
+      .then(setComments);
+  };
 
-    const getSingleComment = (id) => {
-        return fetch(`http://localhost:8088/comments/${id}`)
-            .then(res => res.json())
-            .then(setComment)
-    }
+  const getSingleComment = (id) => {
+    return fetch(`http://localhost:8000/comments/${id}`)
+      .then((res) => res.json())
+      .then(setComment);
+  };
 
-    const getCommentsByPostId = (post_id) => {
-        return fetch(`http://localhost:8088/comments?post_id=${post_id}`)
-            .then(res => res.json())
-            .then(setRelatedComments)
-    }
+  const getCommentsByPostId = (post_id) => {
+    return fetch(`http://localhost:8000/comments?post_id=${post_id}`)
+      .then((res) => res.json())
+      .then(setRelatedComments);
+  };
 
-    const updateComment = (comment) => {
-        return fetch(`http://localhost:8088/comments/${comment.id}`, {
-            method: "PUT",
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(comment)
-        })
-        .then(getComments)
-    }
+  const updateComment = (comment) => {
+    return fetch(`http://localhost:8000/comments/${comment.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(comment),
+    }).then(getComments);
+  };
 
-    const deleteComment = (id) => {
-        return fetch(`http://localhost:8088/comments/${id}`, {
-            method: "DELETE"
-        })
-            .then(getComments)
-    }
-    
-    return(
-        <CommentContext.Provider value={{
-            comment, comments, relatedComments, addComment, getComments, getSingleComment, getCommentsByPostId, updateComment, deleteComment, 
-            setComments, setComment, setRelatedComments
-        }}>
-            {props.children}
-        </CommentContext.Provider>
-    )
-}
+  const deleteComment = (id) => {
+    return fetch(`http://localhost:8000/comments/${id}`, {
+      method: "DELETE",
+    }).then(getComments);
+  };
+
+  return (
+    <CommentContext.Provider
+      value={{
+        comment,
+        comments,
+        relatedComments,
+        addComment,
+        getComments,
+        getSingleComment,
+        getCommentsByPostId,
+        updateComment,
+        deleteComment,
+        setComments,
+        setComment,
+        setRelatedComments,
+      }}
+    >
+      {props.children}
+    </CommentContext.Provider>
+  );
+};
